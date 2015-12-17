d76845cb33a2a40aa8eaa72fe7e52c8ea1b3236f
Nik Silver <nik@niksilver.com>
2015-12-17 23:57:21 +0000

    Bubble now correctly positioned according to its model.

--- Bubbles.elm
+++ Bubbles.elm
@@ -5,7 +5,7 @@ import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 
-type alias Model = { x : Int, y : Int }
+type alias Model = { x : Float, y : Float }
 
 type Action = Empty
 
@@ -17,4 +17,5 @@ view : Signal.Address Action -> Model -> Form
 view address model =
     circle 180
         |> filled darkBrown
+        |> move (model.x, model.y)
 

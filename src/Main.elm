c09390dc79db8e36bb80f99c13b381d0176e2ede
Nik Silver <nik@niksilver.com>
2015-12-22 16:08:31 +0000

    More bubbles, just for fun

--- src/Main.elm
+++ src/Main.elm
@@ -4,8 +4,18 @@ import StartApp.Simple exposing (start)
 
 bubble1Model = { x = 340, y = 200, size = 180, colour = "red" }
 bubble2Model = { x = 450, y = 250, size = 100, colour = "green" }
+bubble3Model = { x = 480, y = 350, size = 80, colour = "red" }
+bubble4Model = { x = 400, y = 400, size = 60, colour = "green" }
 
-model = { width = 800, height = 600, bubbles = [bubble1Model, bubble2Model] }
+model = { width = 800
+    , height = 600
+    , bubbles =
+        [bubble1Model
+        , bubble2Model
+        , bubble3Model
+        , bubble4Model
+        ]
+    }
 
 main =
     start

9b91b8ea114ab9b99c9dd9f2d1e8c634b6f39e9e
Nik Silver <nik@niksilver.com>
2015-12-27 22:49:04 +0000

    More explicit about imports and exports

--- src/PairCounter.elm
+++ src/PairCounter.elm
@@ -1,20 +1,27 @@
-module PairCounter where
+module PairCounter
+    ( Idable, Counter
+    , emptyCounter, countOf, inc
+    , allPairs
+    )
+    where
 
 import List exposing (append, reverse)
 import Maybe exposing (withDefault)
-import Dict exposing (..)
+import Dict exposing (Dict, empty, get, insert)
 
 -- Something that tracks counts of pairs of records which have a String id.
+-- The order of pairs doesn't matter, so setting or getting x,y is the
+-- same as that operation on y,x.
 
 type alias Idable a = { a | id : String }
 
 type Counter = Counter (Dict (String, String) Int)
 
-emptyDict : Dict (String, String) Int
-emptyDict = empty
+emptyDict' : Dict (String, String) Int
+emptyDict' = empty
 
 emptyCounter : Counter
-emptyCounter = Counter emptyDict
+emptyCounter = Counter emptyDict'
 
 -- Return the count of the given pair
 
@@ -22,7 +29,8 @@ countOf : Idable a -> Idable a -> Counter -> Int
 countOf x y (Counter dict) =
     dict |> get (x.id, y.id) |> withDefault 0
 
--- Increment a counter
+-- Increment a counter.
+-- Incrementing x,y will also increment y,x
 
 inc : Idable a -> Idable a -> Counter -> Counter
 inc x y counter =
@@ -49,14 +57,14 @@ allPairs' : List a -> List a -> List (a, a) -> List (a, a)
 allPairs' next done accum =
     case next of
         hd :: tl ->
-            allPairs' tl (hd :: done) (append (distribute hd done) accum)
+            allPairs' tl (hd :: done) (append (distribute' hd done) accum)
         [] ->
             accum
 
 -- Distribute a seed across others to create pairs.
 -- E.g. distribute 7 [1,2,3] == [(7,1), (7,2), (7,3)]
 
-distribute : a -> List a -> List (a, a)
-distribute seed others =
+distribute' : a -> List a -> List (a, a)
+distribute' seed others =
     List.map (\other -> (seed, other)) others
 

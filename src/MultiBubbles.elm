2568e8865d7b93c8b0aa3d7d728ad4f2f048fba2
Nik Silver <nik@niksilver.com>
2015-12-31 01:29:41 +0000

    Refactoring of MultiBubble complete

--- src/MultiBubbles.elm
+++ src/MultiBubbles.elm
@@ -39,18 +39,11 @@ updateAll : Model -> Model
 updateAll model =
     map (\mod -> Phys.update Phys.Move mod) model
 
--- Create a forwarding address for the PhysicsBubble.view which will enable
--- its actions to be sent to and interpretted by this update mechanism.
-
-fwdingAddress : Address Action -> String -> Address Phys.Action
-fwdingAddress address id =
-    forwardTo address (\a -> Direct id a )
-
 -- A view of a PhysicsBubble, using an address at this level of the architecture
 
 fwdingView : Address Action -> Phys.Model -> Svg
 fwdingView address physModel =
-    Phys.view (fwdingAddress address physModel.bubble.id) physModel
+    Phys.view (forwardTo address (Direct physModel.bubble.id)) physModel
 
 view : Address Action -> Model -> List Svg
 view address model =

7419b47b75c92cc91e75dc2d743fc777aa648dec
Nik Silver <nik@niksilver.com>
2016-01-16 21:38:43 +0000

    World can identify old, new and persisting tags after a fetch

--- src/World.elm
+++ src/World.elm
@@ -1,4 +1,8 @@
-module World where
+module World
+    ( Model, Action
+    , update, view
+    , Diff, tagDiff
+    ) where
 
 import Constants exposing (Id, Tags, springStrength)
 import Context exposing (Context, forwardTo)
@@ -18,6 +22,11 @@ type Action
     = Direct MB.Action
     | Tick Time
     | NewTags (List Tags)
+
+-- The difference between tags already in the world and tags fetched
+-- via the API.
+
+type alias Diff = { old : Tags, new : Tags, both : Tags }
 
 update : Action -> Model -> Model
 update action model =
@@ -39,4 +48,16 @@ update action model =
 view : Context Action -> Model -> List Svg
 view context model =
     MB.view (forwardTo context Direct) model.bubbles
+
+-- Find the difference between tags represented in the world
+-- and tags fetched by the API
+
+tagDiff : Tags -> Tags -> Diff
+tagDiff current latest =
+    let
+        old = List.filter (\e -> not(List.member e latest)) current
+        new = List.filter (\e -> not(List.member e current)) latest
+        both = List.filter (\e -> List.member e current) latest
+    in
+        Diff old new both
 
--- tests/TestAll.elm
+++ tests/TestAll.elm
@@ -4,6 +4,7 @@ import TagFetcherTest exposing (all)
 import PairCounterTest exposing (all)
 import SpringsTest exposing (all)
 import SizesTest exposing (all)
+import WorldTest exposing (all)
 
 import Graphics.Element exposing (Element)
 import ElmTest exposing (elementRunner, suite)
@@ -17,5 +18,6 @@ main =
             , PairCounterTest.all
             , SpringsTest.all
             , SizesTest.all
+            , WorldTest.all
             ]
         )

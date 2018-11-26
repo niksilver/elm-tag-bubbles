b3b2a3a1e91e450f8a8ded1df72c62fa82f9f9ea
Nik Silver <nik@niksilver.com>
2018-11-26 15:12:55 +0000

    Started migrating to Elm 0.19

--- tests/TestAll.elm
+++ tests/TestAll.elm
@@ -1,15 +1,15 @@
 module TestAll where
 
-import TagFetcherTest
-import PairCounterTest
-import SpringsTest
-import SizesTest
-import MultiBubblesTest
-import WorldTest
-import LabelTest
-import StatusTest
+-- import TagFetcherTest
+-- import PairCounterTest
+-- import SpringsTest
+-- import SizesTest
+-- import MultiBubblesTest
+-- import WorldTest
+-- import LabelTest
+-- import StatusTest
 
-import Graphics.Element exposing (Element)
+-- import Graphics.Element exposing (Element)
 import ElmTest exposing (elementRunner, suite)
 
 
@@ -17,13 +17,13 @@ main : Element
 main = 
     elementRunner
         (suite "All tests"
-            [ TagFetcherTest.all
-            , PairCounterTest.all
-            , SpringsTest.all
-            , SizesTest.all
-            , MultiBubblesTest.all
-            , WorldTest.all
-            , LabelTest.all
-            , StatusTest.all
+            [ -- TagFetcherTest.all
+            -- , PairCounterTest.all
+            -- , SpringsTest.all
+            -- , SizesTest.all
+            -- , MultiBubblesTest.all
+            -- , WorldTest.all
+            -- , LabelTest.all
+            -- , StatusTest.all
             ]
         )
